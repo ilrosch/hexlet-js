51056f7c04d64f8bde7ab8fd8d9ed4e3ddcb756b
Ilya Roschikhmarov <ilrosch@mail.ru>
2023-10-18 15:24:08 +0300

    work with lodash

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import _ from "lodash";
+
+console.log(_.chunk([1, 2, 4, 4], 2));
+console.log(_.intersection([1, 2, 3, 4], [1, 3, 5, 4, 7, 3]));
+console.log(_.capitalize('hexlet'));
+console.log(_.last(['one', 'two']));
+
+
+
 console.log('Hello, Hexlet!');
