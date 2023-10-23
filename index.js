c6cb79e352a9c7e9379ce1a2c2d6794331af4b0a
Ilya Roschikhmarov <ilrosch@mail.ru>
2023-10-23 09:37:44 +0300

    beatiful code with prettier

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import _ from "lodash";
 
 console.log(_.chunk([1, 2, 4, 4], 2));
 console.log(_.intersection([1, 2, 3, 4], [1, 3, 5, 4, 7, 3]));
-console.log(_.capitalize('hexlet'));
-console.log(_.last(['one', 'two']));
+console.log(_.capitalize("hexlet"));
+console.log(_.last(["one", "two"]));
 
-
-
-console.log('Hello, Hexlet!');
+console.log("Hello, Hexlet!");
